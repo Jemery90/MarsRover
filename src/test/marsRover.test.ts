a76fddb4b51aca796d706d71153098105a0fd71d
Josh Emery <josh.emery@smartfleet.co.uk>
2022-03-11 10:32:33 +0000

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/marsRover.test.ts
+++ src/test/marsRover.test.ts
@@ -1,7 +1,7 @@
 class MarsRover {
-    private xCoordinate: number = 0;
-    private yCoordinate: number = 0;
-    private direction: string = 'N';
+    private xCoordinate: number;
+    private yCoordinate: number;
+    private direction: string;
 
     constructor(xCoordinate: number, yCoordinate: number, direction: string) {
            this.xCoordinate = xCoordinate;
@@ -9,8 +9,14 @@ class MarsRover {
            this.direction = direction;
     }
     move(command: string){
+        if (this.direction === 'W') {
+            this.xCoordinate --;
+    } else {
         this.yCoordinate -- ;
     }
+    
+}
+
     getCoordinate():string{
         return this.xCoordinate + ':' + this.yCoordinate;
     }
@@ -18,17 +24,25 @@ class MarsRover {
 
 describe('MarsRover', () => {
     it('should be able to initialise a new Mars Rover', () => {
-        const marsRover = new MarsRover(0, 0, 'S');
+        const marsRover = new MarsRover(1, 1, 'S');
        expect(marsRover).toBeDefined();
     });
-    it('First move', () => {
-        const marsRover = new MarsRover(0, 0, 'S');
+describe('Given a move command', () => {
+    it('When the rover is facing south it should move south', () => {
+        const marsRover = new MarsRover(1, 1, 'S');
         marsRover.move('M');
-       expect(marsRover.getCoordinate()).toBe('0:-1');
+       expect(marsRover.getCoordinate()).toBe('1:0');
     });
-    it('Second move', () => {
-        const marsRover = new MarsRover(0, 0, 'W');
+    it('When the rover is facing west it should move west', () => {
+        const marsRover = new MarsRover(1, 1, 'W');
         marsRover.move('M');
-       expect(marsRover.getCoordinate()).toBe('-1:0');
+       expect(marsRover.getCoordinate()).toBe('0:1');
     });
+    it('When the rover is facing east it should move east', () => {
+        const marsRover = new MarsRover(1, 1, 'E');
+        marsRover.move('E');
+       expect(marsRover.getCoordinate()).toBe('2:1');
+    });
+} )    
+
 });
