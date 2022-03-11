2edf7d1fbb457fafe5175a3adb0d6ca2fe0459ab
Mahijani <mahima.jani@cazoo.com>
2022-03-11 11:13:46 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/marsRover.test.ts
+++ src/test/marsRover.test.ts
@@ -8,11 +8,27 @@ class MarsRover {
            this.yCoordinate = yCoordinate;
            this.direction = direction;
     }
+    move(command: string){
+        this.yCoordinate -- ;
+    }
+    getCoordinate():string{
+        return this.xCoordinate + ':' + this.yCoordinate;
+    }
 }
 
 describe('MarsRover', () => {
     it('should be able to initialise a new Mars Rover', () => {
         const marsRover = new MarsRover(0, 0, 'S');
        expect(marsRover).toBeDefined();
+    });
+    it('First move', () => {
+        const marsRover = new MarsRover(0, 0, 'S');
+        marsRover.move('M');
+       expect(marsRover.getCoordinate()).toBe('0:-1');
+    });
+    it('Second move', () => {
+        const marsRover = new MarsRover(0, 0, 'W');
+        marsRover.move('M');
+       expect(marsRover.getCoordinate()).toBe('-1:0');
     });
 });
